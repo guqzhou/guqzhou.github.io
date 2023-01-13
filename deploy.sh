631ceb695d0591ab7827da5192e7f9a15fdc3805
guqzhou <guqzhou@163.com>
2023-01-13 14:24:59 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -13,17 +13,17 @@ cd docs/.vuepress/dist
 # echo 'guqzhou/github.io' > CNAME 自定义域名
 if [ -z "$GITEE_RSA_PRIVATE_KEY" ]; then
   msg='deploy'
-  githubUrl= git@github.com:guqzhou/guqzhou.github.io.git
+  githubUrl=git@github.com:guqzhou/guqzhou.github.io.git 
 else
   msg='来自github actions的自动部署'
-  githubUrl = https://guqzhou:${GITEE_RSA_PRIVATE_KEY}@github.com/guqzhou/guqzhou.github.io.git # -z 字符串 长度为0则为true；$GITEE_RSA_PRIVATE_KEY来自于github仓库`Settings/Secrets`设置的私密环境变量
+  githubUrl=https://guqzhou:${GITEE_RSA_PRIVATE_KEY}@github.com/guqzhou/guqzhou.github.io.git # -z 字符串 长度为0则为true；$GITEE_RSA_PRIVATE_KEY来自于github仓库`Settings/Secrets`设置的私密环境变量
   git config --global user.name "guqzhou"
   git config --global user.email "guqzhou@163.com"
 fi
 git init
 git add -A
 git commit -m "${msg}"
-git push -f git@github.com:guqzhou/guqzhou.github.io.git master:gh-pages # 推送到github
+git push -f $githubUrl master:gh-pages # 推送到github
 
 
 # deploy to coding
