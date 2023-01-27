e37f9d3b6540b0cdf5f0323926a68088a649a8ca
guqzhou <guqzhou@163.com>
2023-01-28 07:18:58 +0800

    更新内容

--- baiduPush.sh
+++ baiduPush.sh
@@ -5,5 +5,6 @@ set -e
 
 # 百度链接推送
 curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://guqzhou.github.io&token=EiuLL0XL2GlcTZ3n"
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://guqzhou.gitee.io&token=EiuLL0XL2GlcTZ3n"
 
 rm -rf urls.txt # 灭迹
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,7 +11,7 @@ module.exports = {
     ['link', { rel: 'icon', href: '/img/logo.ico' }], //favicons，资源放在public文件夹
     ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/aplayer@1.10.1/dist/APlayer.min.css' }],
     ['meta', { name: 'keywords', content: '前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown'}],
-    ['meta', { name: 'baidu-site-verification', content: 'code-jIg3DvXrGe'}],// 百度统计博主验证
+    // ['meta', { name: 'baidu-site-verification', content: 'code-jIg3DvXrGe'}],// 百度统计博主验证
     ['meta', {name: "baidu-site-verification", content: "codeva-y12HAIMG1M" }],
     ['meta', { name: 'theme-color', content: '#11a8cd'}], // 移动浏览器主题颜色
     ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no'}], // 移动端阻止页面缩放
