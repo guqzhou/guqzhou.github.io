a952fc124e92e76c4113f7334cc86615b263875f
guqzhou <guqzhou@163.com>
2023-02-06 07:47:39 +0800

    更新内容

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -125,6 +125,9 @@ module.exports = {
       globalUIComponents: ["Twikoo"] // 2.x 版本 globalUIComponents 改名为 clientAppRootComponentFiles
     }],
 
+    [
+      'reading-progress'
+    ],
 
     ['thirdparty-search', { // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
       thirdparty: [ // 可选，默认 []
